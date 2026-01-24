4e73894f9f310a6be07d9e6cd1c8ca379241f677
Jeyster <gaurat.mathieu@gmail.com>
2026-01-24 19:31:52 +0100

    Add routerLinks to ItemInfosBadges

--- src/app/shared/components/item-infos/item-infos.ts
+++ src/app/shared/components/item-infos/item-infos.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Item } from '../../interfaces/item';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-item-infos',
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink,
+    MatTooltip
   ],
   templateUrl: './item-infos.html',
   styleUrls: [
